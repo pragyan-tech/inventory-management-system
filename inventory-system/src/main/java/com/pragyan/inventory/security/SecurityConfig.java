1f8eddcea29b63bf77a8ad395a6fbfce549cfa65
acesvulcan14 <ozapragyan@gmail.com>
2026-01-22 16:28:04 +0530

    "Feat:Implemented Stateless JWT authentication and Security"

--- inventory-system/src/main/java/com/pragyan/inventory/security/SecurityConfig.java
+++ inventory-system/src/main/java/com/pragyan/inventory/security/SecurityConfig.java
@@ -3,16 +3,24 @@ package com.pragyan.inventory.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
+    private final JwtAuthFilter authFilter;
+
+    public SecurityConfig(JwtAuthFilter authFilter) {
+        this.authFilter = authFilter;
+    }
 
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
@@ -23,20 +31,28 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(configurer ->
                 configurer
-                        .requestMatchers(HttpMethod.GET, "/api/products").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/products/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/categories/**").hasRole("EMPLOYEE")
 
-                        .requestMatchers(HttpMethod.POST, "/api/products").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/products").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
 
+                        // PROTECTED ENDPOINTS (Your existing RBAC rules)
+                        .requestMatchers(HttpMethod.GET, "/api/products").hasAnyRole("EMPLOYEE", "MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/products/**").hasAnyRole("EMPLOYEE", "MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/products").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/products").hasAnyRole("MANAGER", "ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/products/**").hasRole("ADMIN")
         );
 
-        http.httpBasic(Customizer.withDefaults());
-
         http.csrf(csrf -> csrf.disable());
 
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
